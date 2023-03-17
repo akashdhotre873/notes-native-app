d22e3449f5187c718c25bc76993e2e66b1e93480
Akash Dhotre <akashdhotre873@gmail.com>
2023-03-18 01:51:49 +0530

    moved constants file to helpers folder

--- helpers/constants.js
+++ helpers/constants.js
@@ -2,6 +2,7 @@ export const promptCategoryType = {
   CREATE_PASSWORD_PROMPT: "create password prompt",
   CONFIRM_PASSWORD_PROMPT: "confirm password prompt",
   ERROR_PROMPT: "error prompt",
+  EXIT_WITHOUT_SAVING: "exit without saving",
 };
 
 export const errorMessages = {
