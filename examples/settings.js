8c5898bdb94cf11d12d7a8dd7a689569f6c5ccbc
Akash Dhotre <akashdhotre873@gmail.com>
2023-12-13 23:38:27 +0530

    added new content icon position setting example to examples

--- examples/settings.js
+++ examples/settings.js
@@ -1,5 +1,10 @@
-import { settingTypes, timeFormats } from '../helpers/constants';
+import {
+  newContentIconPosition,
+  settingTypes,
+  timeFormats,
+} from '../helpers/constants';
 
 const settings = {
   [settingTypes.TIME_FORMAT]: timeFormats[0].format,
+  [settingTypes.NEW_CONTENT_ICON_POSITION]: newContentIconPosition.RIGHT,
 };
