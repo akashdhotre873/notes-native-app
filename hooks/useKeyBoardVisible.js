65b5f2427cc139fb4d59701c7d774a0a373c185a
Akash Dhotre <akashdhotre873@gmail.com>
2023-12-16 02:36:49 +0530

    removed unnecessary commetns

--- hooks/useKeyBoardVisible.js
+++ hooks/useKeyBoardVisible.js
@@ -8,15 +8,11 @@ export const useKeyBoardVisible = () => {
   useEffect(() => {
     const keyboardDidShowListener = Keyboard.addListener(
       'keyboardDidShow',
-      () => {
-        setKeyboardVisible(true); // or some other action
-      }
+      () => setKeyboardVisible(true)
     );
     const keyboardDidHideListener = Keyboard.addListener(
       'keyboardDidHide',
-      () => {
-        setKeyboardVisible(false); // or some other action
-      }
+      () => setKeyboardVisible(false)
     );
 
     return () => {
