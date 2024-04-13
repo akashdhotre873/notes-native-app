f88d95b11b9fdb3b235df373c9168fb19c291f0a
Akash Dhotre <akashdhotre873@gmail.com>
2024-04-14 00:45:19 +0530

    updated settings example object

--- examples/settings.js
+++ examples/settings.js
@@ -1,4 +1,5 @@
 import {
+  colorType,
   newContentIconPosition,
   settingTypes,
   timeFormats,
@@ -7,4 +8,14 @@ import {
 const settings = {
   [settingTypes.TIME_FORMAT]: timeFormats[0].format,
   [settingTypes.NEW_CONTENT_ICON_POSITION]: newContentIconPosition.RIGHT,
+  [settingTypes.COLORS]: {
+    [colorType.PRIMARY_COLOR]: '#006efe',
+    [colorType.LOCKED_COLOR]: '#fe9a03',
+    [colorType.UNLOCKED_COLOR]: '#01eb00',
+    [colorType.SETTING_BOX_BORDER_Color]: 'grey',
+    [colorType.SETTING_BOX_BACKGROUND_Color]: 'white',
+    [colorType.NEW_CONTENT_ICON_COLOR]: '#006efe',
+    [colorType.ICON_PRIMARY_COLOR]: 'black',
+    [colorType.HEADER_TEXT_COLOR]: 'black',
+  },
 };
