9efdaec0eb8034a14c764825a2980f565611be4a
Akash Dhotre <akashdhotre873@gmail.com>
2024-11-30 23:53:34 +0530

    added theme for paper provider

--- App.js
+++ App.js
@@ -1,7 +1,11 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import * as SplashScreen from 'expo-splash-screen';
-import { Provider as PaperProvider } from 'react-native-paper';
+import {
+  Provider as PaperProvider,
+  MD3LightTheme as DefaultTheme,
+  MD3DarkTheme as DarkTheme,
+} from 'react-native-paper';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Provider } from 'react-redux';
 
@@ -22,6 +26,8 @@ import { NoteEditorScreen } from './screens/NoteEditorScreen';
 import { SettingsScreen } from './screens/SettingsScreen';
 import { TodoEditorScreen } from './screens/TodoEditorScreen';
 import store from './store/store';
+import { getColorScheme } from './dux/settings';
+import { colorSchemes } from './helpers/constants';
 
 const Stack = createNativeStackNavigator();
 
@@ -34,35 +40,43 @@ const OtherComponents = () => {
 // Keep the splash screen visible while we fetch resources
 SplashScreen.preventAutoHideAsync();
 
+const StoreContainer = () => {
+  const colorScheme = useShallowEqualSelector(getColorScheme);
+  const theme = colorSchemes.DARK === colorScheme ? DarkTheme : DefaultTheme;
+  return (
+    <PaperProvider theme={theme}>
+      <LoadApp />
+      <ConfigProtectedLayout>
+        <SafeAreaProvider>
+          <NavigationContainer>
+            <StatusBarContainer />
+            <Stack.Navigator screenOptions={{ headerShown: false }}>
+              <Stack.Screen name={HOME_SCREEN_PATH} component={HomeScreen} />
+              <Stack.Screen
+                name={NOTE_EDITOR_SCREEN_PATH}
+                component={NoteEditorScreen}
+              />
+              <Stack.Screen
+                name={TODO_EDITOR_SCREEN_PATH}
+                component={TodoEditorScreen}
+              />
+              <Stack.Screen
+                name={SETTINGS_SCREEN_PATH}
+                component={SettingsScreen}
+              />
+            </Stack.Navigator>
+            <OtherComponents />
+          </NavigationContainer>
+        </SafeAreaProvider>
+      </ConfigProtectedLayout>
+    </PaperProvider>
+  );
+};
+
 export default function App() {
   return (
     <Provider store={store}>
-      <PaperProvider>
-        <LoadApp />
-        <ConfigProtectedLayout>
-          <SafeAreaProvider>
-            <NavigationContainer>
-              <StatusBarContainer />
-              <Stack.Navigator screenOptions={{ headerShown: false }}>
-                <Stack.Screen name={HOME_SCREEN_PATH} component={HomeScreen} />
-                <Stack.Screen
-                  name={NOTE_EDITOR_SCREEN_PATH}
-                  component={NoteEditorScreen}
-                />
-                <Stack.Screen
-                  name={TODO_EDITOR_SCREEN_PATH}
-                  component={TodoEditorScreen}
-                />
-                <Stack.Screen
-                  name={SETTINGS_SCREEN_PATH}
-                  component={SettingsScreen}
-                />
-              </Stack.Navigator>
-              <OtherComponents />
-            </NavigationContainer>
-          </SafeAreaProvider>
-        </ConfigProtectedLayout>
-      </PaperProvider>
+      <StoreContainer />
     </Provider>
   );
 }
